b7f93149118fc5daee68bfb19790f6c06c1cc51f
dkorneva <dariakorneva20@gmail.com>
2026-07-20 13:36:00 +0300

    feat: добавлены страница NotFoundPage, props для QuestionCard, динамические маршруты

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { MainLayout } from './components/MainLayout'
 import { HomePage } from './pages/HomePage'
+import { NotFoundPage } from './pages/NotFoundPage'
 
 function App() {
   return (
@@ -12,6 +13,11 @@ function App() {
         <Route path="/main" element={<div>main component</div>} />
         <Route path="/forbidden" element={<div>forbidden</div>} />
         <Route path="/addquestion" element={<div>add question</div>} />
+        {/* для динамики в react-router необходимо поставить :id, т.е. связываем задекларированный маршрут с конкретным маршрутом из QuestionCard*/}
+        <Route path="/question/:id" element={<div>QUESTION PAGE</div>} />
+
+        {/* path="*" необходим для всех остальных маршрутов, которые не были задекларированы */}
+        <Route path="*" element={<NotFoundPage/>}></Route>
       </Route>
     </Routes>
   </BrowserRouter>)
--- src/components/QuestionCard/QuestionCard.jsx
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -1,26 +1,27 @@
 import {} from 'react'
 import { Button } from '../Button'
 import cls from './QuestionCard.module.css'
+import { useNavigate } from 'react-router-dom'
 
-export const QuestionCard = () => {
+export const QuestionCard = ({ card }) => {
+  const navigate = useNavigate();
 	return (
 		<div className={cls.card}>
 			<div className={cls.cardLabels}>
-				<div>Level: 1</div>
-				<div>Not Completed</div>
+				<div>Level: {card.level}</div>
+				<div>{card.completed ? "Completed" : "Not Completed"}</div>
 			</div>
 
-			<h5 className={cls.cardTitle}>Что такое JSX?</h5>
+			<h5 className={cls.cardTitle}>{card.question}</h5>
 
 			<div className={cls.cardAnswers}>
 				<label>short answer: </label>
 				<p className={cls.cardAnswer}>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore,
-					magnam?
+					{card.answer}
 				</p>
 			</div>
 
-			<Button onClick={() => {}}>View</Button>
+			<Button onClick={() => navigate(`/question/${card.id}`)}>View</Button>
 		</div>
 	)
 }
