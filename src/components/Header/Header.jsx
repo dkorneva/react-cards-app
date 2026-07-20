9bda0120dee8de7d4d0bc2baf58b6c2f3b1a85bc
dkorneva <dariakorneva20@gmail.com>
2026-07-20 11:53:06 +0300

    feat: добавлен header и обновлены стили button

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import ReactLogo from "../../assets/react.svg";
+import { Button } from "../Button";
+import cls from "./Header.module.css";
+
+export const Header = () => {
+  return (
+  <header className={cls.header}>
+      <p>
+        <img src={ReactLogo} alt="react logo" />
+        <span>React Cards</span>
+      </p>
+
+      <div className={cls.headerButtons}>
+        <Button isActive>Add</Button>
+        <Button>Login</Button>
+      </div>
+    </header>
+    );
+}
