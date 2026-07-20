c29a36879007f1ffd86690339d7923c51b258fd9
dkorneva <dariakorneva20@gmail.com>
2026-07-20 16:25:30 +0300

    feat: настроен json сервер

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,67 +1,38 @@
-import { } from "react";
-import cls from "./HomePage.module.css"
+import { useEffect, useState } from "react";
+// import cls from "./HomePage.module.css"
 import { QuestionCard } from "../../components/QuestionCard";
-
-const cards = [
-    {
-      "id": "1",
-      "question": "Что такое React?",
-      "answer": "React — это библиотека для создания пользовательских интерфейсов.",
-      "description": "React — это JavaScript-библиотека, разработанная Facebook, которая используется для построения UI с компонентным подходом. React позволяет вам создавать пользовательские интерфейсы из отдельных частей, называемых компонентами.",
-      "resources": [
-        "https://react.dev",
-        "https://react.dev/reference/react"
-      ],
-      "level": 1,
-      "completed": true,
-      "editDate": "03.02.2025, 19:49"
-    },
-    {
-      "id": "2",
-      "question": "Что такое JSX?",
-      "answer": "JSX — это синтаксическое расширение JavaScript для React.",
-      "description": "JSX позволяет писать HTML-подобный код в JavaScript, который затем транспилируется в вызовы `React.createElement`. Он облегчает создание и визуальное представление структуры компонентов.",
-      "resources": [
-        "https://react.dev/learn/writing-markup-with-jsx"
-      ],
-      "level": 2,
-      "completed": false,
-      "editDate": "03.02.2025, 20:25"
-    },
-    {
-      "id": "3",
-      "question": "Какой основной принцип работы React?",
-      "answer": "React использует Virtual DOM для оптимизации рендеринга.",
-      "description": "React поддерживает концепцию Virtual DOM — это представление реального DOM в памяти. Когда состояние компонента изменяется, React сравнивает Virtual DOM с предыдущим состоянием и обновляет только измененные элементы на странице.",
-      "resources": [
-        "https://react.dev/learn/render-and-commit"
-      ],
-      "level": 2,
-      "completed": false,
-      "editDate": "03.02.2025, 19:01"
-    },
-    {
-      "id": "4",
-      "question": "Как создать компонент в React?🍩",
-      "answer": "Компонент можно создать как функцию или класс.",
-      "description": "Функциональные компоненты — это функции, принимающие `props` и возвращающие JSX. Классовые компоненты — это классы, наследуемые от `React.Component`, с методом `render`.",
-      "resources": [
-        "https://react.dev/learn/your-first-component"
-      ],
-      "level": 1,
-      "completed": false,
-      "editDate": "21.02.2025, 20:26"
-    },
-]
+import { API_URL } from "../../constants";
 
 export const HomePage = () => {
+  const [questions, setQuestions] = useState([]);
+
+  const getQuestions = async () => {
+		try {
+      const response = await fetch(`${API_URL}/react`);
+      const questions = await response.json();
+
+      setQuestions(questions);
+
+      console.log("questions", questions);
+		} catch (error) {
+      console.error(error)
+    }
+	}
+
+  // useEffect ничего не возваращает, в качестве параметров у него callback-функция и массив зависимостей, при которых callback-функция будет отрабатывать, если оставить массив зависимостей пустым, callback-функция отработает один раз - когда смонтируется компонент
+  useEffect(() => {
+    getQuestions();
+  }, [])
+
+  // getQuestions(); // при вызове таким образом, будет двойной рендер - по количеству запросов, этого можно и нужно избежать при помощи хука useEffect;
+
   return (
     // в react обязателен родительский элемент, в который оборачиваются дочерние элементы
     // в данном случае таким элементом служит div с классом test-class, удалить его нельзя
     // но в react есть концепция виртуального родительского класса - react fragment
     // ниже представлен вариант без импортов, но есть также варианты с тегами React.Fragment или Fragment
     <> 
-    {cards.map((card, index) => {
+    {questions.map((card, index) => {
       return <QuestionCard card={card} key={index}/>
     })}
     </>
