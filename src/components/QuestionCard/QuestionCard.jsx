fc6e98aa7de48c7c810060121f13d9de8684a8fd
dkorneva <dariakorneva20@gmail.com>
2026-07-20 21:07:37 +0300

    feat: добавлен бейдж в карточки

--- src/components/QuestionCard/QuestionCard.jsx
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -2,23 +2,26 @@ import {} from 'react'
 import { Button } from '../Button'
 import cls from './QuestionCard.module.css'
 import { useNavigate } from 'react-router-dom'
+import { Badge } from '../Badge'
 
 export const QuestionCard = ({ card }) => {
-  const navigate = useNavigate();
+	const navigate = useNavigate()
+
+	const levelVariant = card.level === 1 ? "primary" : card.level === 2 ? "warning" : "alert"
+	const completedVariant = card.completed ? 'success' : 'primary' 
+
 	return (
 		<div className={cls.card}>
 			<div className={cls.cardLabels}>
-				<div>Level: {card.level}</div>
-				<div>{card.completed ? "Completed" : "Not Completed"}</div>
+				<Badge variant={levelVariant}>Level: {card.level}</Badge>
+				<Badge variant={completedVariant}>{card.completed ? 'Completed' : 'Not Completed'}</Badge>
 			</div>
 
 			<h5 className={cls.cardTitle}>{card.question}</h5>
 
 			<div className={cls.cardAnswers}>
 				<label>short answer: </label>
-				<p className={cls.cardAnswer}>
-					{card.answer}
-				</p>
+				<p className={cls.cardAnswer}>{card.answer}</p>
 			</div>
 
 			<Button onClick={() => navigate(`/question/${card.id}`)}>View</Button>
