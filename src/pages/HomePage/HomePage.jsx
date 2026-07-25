b7000137a751010dd92a7dd1b05c41317e10f76d
dkorneva <dariakorneva20@gmail.com>
2026-07-25 12:05:38 +0300

    feat: добавлена пагинация и скролл

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,63 +1,101 @@
-import { useEffect, useState, useRef, useMemo } from "react";
+import { useEffect, useState, useRef, useMemo } from 'react'
 // import cls from "./HomePage.module.css"
-import { QuestionCard } from "../../components/QuestionCard";
-import { API_URL } from "../../constants";
-import { QuestionCardList } from "../../components/QuestionCardList";
-import { Loader } from '../../components/Loader';
-import { useFetch } from "../../hooks/useFetch";
-import { SearchInput } from "../../components/SearchInput";
-import cls from "./HomePage.module.css"
+import { QuestionCard } from '../../components/QuestionCard'
+import { API_URL } from '../../constants'
+import { QuestionCardList } from '../../components/QuestionCardList'
+import { Loader } from '../../components/Loader'
+import { useFetch } from '../../hooks/useFetch'
+import { SearchInput } from '../../components/SearchInput'
+import {Button} from '../../components/Button'
+import cls from './HomePage.module.css'
+
+const DEFAULT_PER_PAGE = 10
 
 export const HomePage = () => {
-  // большинство хуков React можно вызвать только на верхнем уровне
-  // т.е., например, нельзя вызвать useState внутри getQuestions
-  const [questions, setQuestions] = useState([]);
-  const [searchValue, setSearchValue] = useState(""); // управляемый input 
-  const [sortSelectValue, setSortSelectValue] = useState("");
+	// большинство хуков React можно вызвать только на верхнем уровне
+	// т.е., например, нельзя вызвать useState внутри getQuestions
+	const [searchParams, setSearchParams] = useState(
+		`?_page=1&_per_page=${DEFAULT_PER_PAGE}`,
+	)
+	const [questions, setQuestions] = useState({})
+	const [searchValue, setSearchValue] = useState('') // управляемый input
+	const [sortSelectValue, setSortSelectValue] = useState('')
+  const controlsContainerRef = useRef();
 
-  // const inputRef = useRef(); // данная ссылка не является состоянием, а представлет из себя ссылку на какой-то элемент, значение данного элемента сохраняется между перерисовками, оно постоянное и не будет сбрасываться
-  // с помощью данного референса можно также управлять скроллом, получать данные у input (например, делать какую-либо константу)
-  // данный способ является прямой альтернативой selectQuery из JS
+  const getActivePageNumber = () => {
+    return questions.next === null ? questions.last : questions.next - 1;
+  }
 
-  const [getQuestions, isLoading, error] = useFetch(async (url) => {
-    const response = await fetch(`${API_URL}/${url}`);
+	// const inputRef = useRef(); // данная ссылка не является состоянием, а представлет из себя ссылку на какой-то элемент, значение данного элемента сохраняется между перерисовками, оно постоянное и не будет сбрасываться
+	// с помощью данного референса можно также управлять скроллом, получать данные у input (например, делать какую-либо константу)
+	// данный способ является прямой альтернативой selectQuery из JS
 
-    if (!response.ok) {
-      throw new Error(`Ошибка загрузки: ${response.status}`);
-    }
+	const [getQuestions, isLoading, error] = useFetch(async url => {
+		const response = await fetch(`${API_URL}/${url}`)
 
-    const questions = await response.json();
+		if (!response.ok) {
+			throw new Error(`Ошибка загрузки: ${response.status}`)
+		}
 
-    setQuestions(questions);
-    return questions;
-  })
+		const questions = await response.json()
 
-  const cards = useMemo(() => {
-    return questions.filter(d =>
-			d.question.toLowerCase().includes(searchValue.trim().toLowerCase()),
+		setQuestions(questions)
+		return questions
+	})
+
+	const cards = useMemo(() => {
+		if (questions?.data) {
+			if (searchValue.trim()) {
+				return questions.data.filter(d =>
+					d.question.toLowerCase().includes(searchValue.trim().toLowerCase()),
+				)
+			} else {
+				return questions.data
+			}
+		}
+		return []
+	}, [questions, searchValue])
+
+	// используем useMemo, чтобы pagination не пересчитывался на каждый перерендер, пересчёт будет только при изменении questions
+	const pagintaion = useMemo(() => {
+		const totalCardsCount = questions?.pages || 0
+
+		return Array(totalCardsCount)
+			.fill(0)
+			.map((_, i) => i + 1) // формирование массива с опред. количеством значений с опред. значениями у элементов; кол-во элементов - totalCardsCount, заполнить массив нулями, пройтись по массиву и заполнить его значениями номеров страниц
+	}, [questions])
+
+	// useEffect ничего не возваращает, в качестве параметров у него callback-функция и массив зависимостей, при которых callback-функция будет отрабатывать, если оставить массив зависимостей пустым, callback-функция отработает один раз - когда смонтируется компонент
+	useEffect(() => {
+		getQuestions(`react${searchParams}`)
+	}, [searchParams])
+
+	// getQuestions(); // при вызове таким образом, будет двойной рендер - по количеству запросов, этого можно и нужно избежать при помощи хука useEffect;
+
+	// const testRefHandler = () => {
+	//   console.log("Ref", inputRef.current.value)
+	// }
+
+	const onSearchChangeValueHandler = e => {
+		setSearchValue(e.target.value)
+	}
+
+	const onSortSelectChangeHandler = e => {
+		setSortSelectValue(e.target.value)
+
+		setSearchParams(
+			`?_page=1&_per_page=${DEFAULT_PER_PAGE}&${e.target.value}`,
 		)
-  }, [questions, searchValue]) 
+	}
 
-  // useEffect ничего не возваращает, в качестве параметров у него callback-функция и массив зависимостей, при которых callback-функция будет отрабатывать, если оставить массив зависимостей пустым, callback-функция отработает один раз - когда смонтируется компонент
-  useEffect(() => {
-    getQuestions(`react?${sortSelectValue}`);
-  }, [sortSelectValue])
-
-  // getQuestions(); // при вызове таким образом, будет двойной рендер - по количеству запросов, этого можно и нужно избежать при помощи хука useEffect;
-
-  // const testRefHandler = () => {
-  //   console.log("Ref", inputRef.current.value)
-  // }
-
-  const onSearchChangeValueHandler = (e) => {
-    setSearchValue(e.target.value);
+  const paginationHandler = (e) => {
+    if (e.target.tagName === "BUTTON") {
+      setSearchParams(`?_page=${e.target.textContent}&_per_page=${DEFAULT_PER_PAGE}&${sortSelectValue}`)
+      controlsContainerRef.current.scrollIntoView({behavior: "smooth"}); // у контрола вызываем поле current, у которого вызываем метод js scrollIntoView(), behhavior: "smooth" отвечает за плавность скролла
+    }
   }
 
-  const onSortSelectChangeHandler = (e) => {
-    setSortSelectValue(e.target.value);
-  }
-
-  return (
+	return (
 		// в react обязателен родительский элемент, в который оборачиваются дочерние элементы
 		// в данном случае таким элементом служит div с классом test-class, удалить его нельзя
 		// но в react есть концепция виртуального родительского класса - react fragment
@@ -67,23 +105,42 @@ export const HomePage = () => {
       return <QuestionCard card={card} key={index}/>
     })} */}
 			{/* <input type="text" ref={inputRef}/> неуправляемый input, для получения значения используется ref */}
-			<div className={cls.controlsContainer}>
-        <SearchInput value={searchValue} onChange={onSearchChangeValueHandler}/>
+			<div className={cls.controlsContainer} ref={controlsContainerRef}>
+				<SearchInput
+					value={searchValue}
+					onChange={onSearchChangeValueHandler}
+				/>
 
-        <select value={sortSelectValue} onChange={onSortSelectChangeHandler} className={cls.select}>
-          <option value="">sortby</option>
-          <hr />
-          <option value="_sort=level">level ASC</option>
-          <option value="_sort=-level">level DESC</option>
-          <option value="_sort=completed">completed ASC</option>
-          <option value="_sort=-completed">completed DESC</option>
-        </select>
-      </div>
+				<select
+					value={sortSelectValue}
+					onChange={onSortSelectChangeHandler}
+					className={cls.select}
+				>
+					<option value=''>sortby</option>
+					<hr />
+					<option value='_sort=level'>level ASC</option>
+					<option value='_sort=-level'>level DESC</option>
+					<option value='_sort=completed'>completed ASC</option>
+					<option value='_sort=-completed'>completed DESC</option>
+				</select>
+			</div>
 			{isLoading && <Loader />}
 			{error && <p>{error}</p>}
-      {cards.length === 0 && <p className={cls.noCardsInfo}>No cards...</p>}
 
 			<QuestionCardList cards={cards} />
+
+{/* Для реализации динамической пагинации используется паттерн Event Delegation 
+  Т.е. прослушиватель событий был повешен только на общий блок-враппер, а не накаждый 
+  элемент Button*/}
+			{cards.length === 0 ? (
+				<p className={cls.noCardsInfo}>No cards...</p>
+			) : (
+				<div className={cls.pagintaionContainer} onClick={paginationHandler}>
+					{pagintaion.map(value => {
+						return <Button key={value} isActive={value === getActivePageNumber()}>{value}</Button>
+					})}
+				</div>
+			)}
 		</>
 	)
 }
