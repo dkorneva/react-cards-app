a6756f5c2551fe4988ceb3f6b252f378cfec85b2
dkorneva <dariakorneva20@gmail.com>
2026-07-28 23:27:55 +0300

    ts setup, eslint setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,56 +1,38 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import prettierPlugin from 'eslint-plugin-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
-import prettier from 'eslint-plugin-prettier'
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-
-    extends: [
-      js.configs.recommended,
-      react.configs.flat.recommended,
-      react.configs.flat['jsx-runtime'],
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-
-    languageOptions: {
-      globals: globals.browser,
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-    },
-
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      prettier,
-    },
-
-    rules: {
-      'react/jsx-no-target-blank': 'error',
-      'react/prop-types': 'warn',
-      'prettier/prettier': [
-        "error",
-        {
-          singleQuote: false,
-          printWidth: 130,
-          tabWidth: 2,
-        }
-      ]
-    },
-  },
+	globalIgnores(['dist']),
+	{
+		files: ['**/*.{js,jsx,ts,tsx}'],
+		extends: [
+			js.configs.recommended,
+			tseslint.configs.recommended,
+			reactHooks.configs.flat.recommended,
+			reactRefresh.configs.vite,
+		],
+		plugins: {
+			prettier: prettierPlugin,
+		},
+		languageOptions: {
+			globals: globals.browser,
+		},
+		rules: {
+      "no-console": "warn",
+      "react-refresh/only-export-components": ["warn", {allowConstantExport: true}],
+			'prettier/prettier': [
+				'error',
+				{
+					singleQuote: false,
+					printWidth: 130,
+					tabWidth: 2,
+				},
+			],
+		},
+	},
 ])
